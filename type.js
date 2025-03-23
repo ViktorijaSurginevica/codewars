4067b76444255f86227b0bd229b21e5b068bfc4d
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-23 13:15:44 +0300

    function render

--- type.js
+++ type.js
@@ -1,15 +1,26 @@
+//CRUD
+
 // Проектируем данные
 
 const data = {
-    course: {           // Данные нужны для отображения нашего курса
-        title: "Основы Fron-end разработки"
-        lessons: [
-            {id: 1, title: "Введение в JavaScript", isDOne: false}
-            {id: 2, title: "Операторы, сравнение, ветвление", isDOne: false}
-            {id: 3, title: "Функции", isDone: true}
-            {id: 4, title: "Массивы", isDone: false}
-            {id: 5, title: "Объекты", isDone: false}
-        ]
-    }
-   
+  course: {
+    // Данные нужны для отображения нашего курса
+    title: "Основы Fron-end разработки",
+    lessons: [
+      { id: 1, title: "Введение в JavaScript", isDone: false },
+      { id: 2, title: "Операторы, сравнение, ветвление", isDone: false },
+      { id: 3, title: "Функции", isDone: true },
+      { id: 4, title: "Массивы", isDone: false },
+      { id: 5, title: "Объекты", isDone: false },
+    ],
+  },
+  render() {
+    console.log(this.course); // команда для вывода курса
+  },
+};
+
+// методы, которые будут работать с нашими данными
+
+deleteLesson(lessonId){
+
 }
