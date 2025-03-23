560dbfad00ac71f2c252591ca4af592ae7c3891e
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-23 13:44:10 +0300

    addLesson

--- type.js
+++ type.js
@@ -7,11 +7,11 @@ const data = {
     // Данные нужны для отображения нашего курса
     title: "Основы Fron-end разработки",
     lessons: [
-      { id: 1, title: "Введение в JavaScript", isDone: false },
-      { id: 2, title: "Операторы, сравнение, ветвление", isDone: false },
-      { id: 3, title: "Функции", isDone: true },
-      { id: 4, title: "Массивы", isDone: false },
-      { id: 5, title: "Объекты", isDone: false },
+      { id: crypto.randomUUID, title: "Введение в JavaScript", isDone: true },
+      { id: crypto.randomUUID, title: "Операторы, сравнение, ветвление", isDone: false },
+      { id: crypto.randomUUID, title: "Функции", isDone: false },
+      { id: crypto.randomUUID, title: "Массивы", isDone: false },
+      { id: crypto.randomUUID, title: "Объекты", isDone: false },
     ],
   },
   render() {
@@ -25,8 +25,8 @@ const data = {
     this.render();
   },
 
-  addLesson(lessonId) {
-    const newLesson = {};
+  addLesson(lessonTitle) {
+    const newLesson = { id: 6, title: lessonTitle, isDone: false };
     this.course.lessons.push(newLesson); //
     this.render();
   },
