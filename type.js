b032bf97188eb7b743a9690ae934ebff718e73e7
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-03-23 12:56:06 +0300

    main object

---
+++ type.js
@@ -0,0 +1,15 @@
+// Проектируем данные
+
+const data = {
+    course: {           // Данные нужны для отображения нашего курса
+        title: "Основы Fron-end разработки"
+        lessons: [
+            {id: 1, title: "Введение в JavaScript", isDOne: false}
+            {id: 2, title: "Операторы, сравнение, ветвление", isDOne: false}
+            {id: 3, title: "Функции", isDone: true}
+            {id: 4, title: "Массивы", isDone: false}
+            {id: 5, title: "Объекты", isDone: false}
+        ]
+    }
+   
+}
