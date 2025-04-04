7fb3ac4a2e7097ee42ab142a0a6fbe7bee846f9e
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-04-04 12:38:12 +0300

    model

--- MVC.js
+++ MVC.js
@@ -1,8 +1,18 @@
+const task = {id: 1, title: "learn next lesson", isDone: false}
+const MOCK_TASKS = [
+    { id: 1, title: 'Изучить паттерн MVC', isDone: false },
+    { id: 2, title: 'Подготовить моковые данные', isDone: true },
+]
+
 // хранение данных, бизнес-логика
-const model = {}
+const model = {
+    tasks: MOCK_TASKS
+  }
  
 // отображение данных: рендер списка задач, размещение обработчиков событий
-const view = {}
+const view = {
+    tasks: MOCK_TASKS
+}
  
 // обработка действий пользователя, обновление модели
 const controller = {}
