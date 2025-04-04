721b7f93aeb23b608627f0152f10095eb61b9fb2
”ViktorijaSurginevica” <viafuture89@gmail.com>
2025-04-04 12:38:34 +0300

    view

--- MVC.js
+++ MVC.js
@@ -1,19 +1,30 @@
-const task = {id: 1, title: "learn next lesson", isDone: false}
+const task = { id: 1, title: "learn next lesson", isDone: false };
 const MOCK_TASKS = [
-    { id: 1, title: 'Изучить паттерн MVC', isDone: false },
-    { id: 2, title: 'Подготовить моковые данные', isDone: true },
-]
+  { id: 1, title: "Изучить паттерн MVC", isDone: false },
+  { id: 2, title: "Подготовить моковые данные", isDone: true },
+];
 
 // хранение данных, бизнес-логика
 const model = {
-    tasks: MOCK_TASKS
-  }
- 
+  tasks: MOCK_TASKS,
+};
+
 // отображение данных: рендер списка задач, размещение обработчиков событий
 const view = {
-    tasks: MOCK_TASKS
+  init() {
+    this.renderTasks(model.tasks);
+  },
+  // renderTasks(tasks) { ... },
+};
+
+// Функция инициализации
+function init() {
+  view.init();
+  // здесь может быть код инициализации других модулей
 }
- 
+
+// Вызов функции инициализации при загрузке страницы
+init();
+
 // обработка действий пользователя, обновление модели
-const controller = {}
- 
+const controller = {};
